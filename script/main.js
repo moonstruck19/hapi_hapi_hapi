20a55a58dcf2fc66b1ff1dc3308ad6042bb388b9
moonstruck19 <lamborghininguyn@gmail.com>
2024-11-26 01:38:56 +0700

    Change context of warning

--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
 // trigger to play music in the background with sweetalert
 window.addEventListener('load', () => {
     Swal.fire({
-        title: 'Wanna play music in the background?',
+        title: 'Wanna play music in the background? Use a browser for the best experience.',
         icon: 'warning',
         // showCancelButton: true,
         confirmButtonColor: '#3085d6',
